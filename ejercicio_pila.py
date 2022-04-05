cf6d9eb1b2eb706f7058e36e9d850e2c669a2a4d
walter <belwalterv@gmail.com>
2022-04-05 20:11:02 -0300

    ejercicio pila

--- ejercicio_pila.py
+++ ejercicio_pila.py
@@ -3,19 +3,37 @@ from pila import Pila
 from random import randint
 
 pila1 = Pila()
+pila_aux = Pila()
 
-for i in range(15):
-    num = randint(1, 5)
+for i in range(10):
+    num = randint(1, 100)
     print(num)
     pila1.apilar(num)
 
-x = int(input('ingrese el numero que desae contar ocurrencias '))
+# ejercicio 1
+# x = int(input('ingrese el numero que desae contar ocurrencias '))
 
-contador = 0
+# contador = 0
+# while(not pila1.pila_vacia()):
+#     dato = pila1.desapilar()
+
+#     if(dato == x):
+#         contador += 1
+
+# print('cantidad de ocurrencias:', contador)
+
+# ejercicio 2
 while(not pila1.pila_vacia()):
     dato = pila1.desapilar()
 
-    if(dato == x):
-        contador += 1
+    if(dato % 2 == 0):
+        pila_aux.apilar(dato)
 
-print('cantidad de ocurrencias:', contador)
+while(not pila_aux.pila_vacia()):
+    dato = pila_aux.desapilar()
+    pila1.apilar(dato)
+
+print('elementos pares')
+while(not pila1.pila_vacia()):
+    dato = pila1.desapilar()
+    print(dato)
