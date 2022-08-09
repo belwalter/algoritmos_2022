526fe71650c3e04f55eb7376204b13c1daef028b
walter <belwalterv@gmail.com>
2022-08-09 20:22:04 -0300

    arbol partee 2

--- arbol.py
+++ arbol.py
@@ -21,6 +21,10 @@ def insertar_nodo(arbol, dato):
         insertar_nodo(arbol['der'], dato)
 
 
+def arbol_vacio():
+    return arbol['info'] is None
+
+
 def preorden(arbol):
     if(arbol is not None):
         print(arbol['info'])
@@ -41,15 +45,72 @@ def postorden(arbol):
         postorden(arbol['izq'])
 
 
+def busqueda(arbol, clave):
+    aux = None
+    if arbol is not None and arbol['info'] is not None:
+        if arbol['info'] == clave:
+            aux = arbol
+        elif clave < arbol['info']:
+            aux = busqueda(arbol['izq'], clave)
+        else:
+            aux = busqueda(arbol['der'], clave)
+    return aux
+
+
+
+def remplazar(arbol):
+    aux = None
+    if arbol['der'] is None:
+        aux = arbol['info']
+    else:
+        aux = remplazar(arbol['der'])
+    return aux
+
+
+def eliminar_nodo(arbol, clave):
+    x = None
+    if arbol['info'] is not None:
+        if clave < arbol['info']:
+            x = eliminar_nodo(arbol['izq'], clave)
+        elif clave > arbol['info']:
+            x = eliminar_nodo(arbol['der'], clave)
+        else:
+            x = arbol['info']
+            if arbol['izq'] is None and arbol['der'] is not None:
+                arbol['info'] = arbol['der']['info']
+                arbol['izq'] = arbol['der']['izq']
+                arbol['der'] = arbol['der']['der']
+            elif arbol['der'] is None and arbol['izq'] is not None:
+                arbol['info'] = arbol['izq']['info']
+                arbol['der'] = arbol['izq']['der']
+                arbol['izq'] = arbol['izq']['izq']
+            else:
+                aux = remplazar(arbol['izq'])
+                arbol['info'] = aux
+
+    return x
+
 arbol = nodoArbol()
 
 insertar_nodo(arbol, 19)
 insertar_nodo(arbol, 7)
-insertar_nodo(arbol, 11)
+insertar_nodo(arbol, 1)
 insertar_nodo(arbol, 31)
 insertar_nodo(arbol, 22)
 insertar_nodo(arbol, 45)
 insertar_nodo(arbol, 27)    
 
+inorden(arbol)
 
-postorden(arbol)
+# value = eliminar_nodo(arbol, 31)
+
+# print('valor eliminado', value)
+
+# inorden(arbol)
+
+
+pos = busqueda(arbol, 45)
+if pos:
+    print('asdasd', pos['info'])
+
+# postorden(arbol)
